230566ca6cdac330792cfa04d89343e91397a29a
cerealtwist <ventriloquist225@gmail.com>
2024-05-15 20:16:11 +0700

    Menu: Main Menu
    Create function mainMenu()

--- PerfAssesment.go
+++ PerfAssesment.go
@@ -1,9 +1,28 @@
 package main
 
 import (
-    "fmt"
+	"fmt"
 )
 
+func mainMenu() {
+	var choice int
+
+	for {
+		fmt.Println("======================================")
+		fmt.Println("Aplikasi Pengolahan Data Kinerja Karyawan")
+		fmt.Println("======================================")
+		fmt.Println("1. Kelola Data Karyawan")
+		fmt.Println("2. Kelola Log Pekerjaan Karyawan")
+		fmt.Println("3. Lihat Rekap Data")
+		fmt.Println("0. Exit")
+		fmt.Println("======================================")
+		fmt.Print("Pilih menu: ")
+
+		fmt.Scan(&choice)
+
+	}
+}
+
 func main() {
-	// buat program main
+	mainMenu()
 }
